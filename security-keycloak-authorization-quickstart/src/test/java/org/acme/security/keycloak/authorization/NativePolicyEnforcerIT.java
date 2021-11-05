77c42ffa3245c2e03fbc0fca7c1f488de9fb5378
Sebastien Dionne <Sebastien.Dionne@bidgroup.ca>
2021-11-05 07:56:33 -0400

    Standardize the examples

--- security-keycloak-authorization-quickstart/src/test/java/org/acme/security/keycloak/authorization/NativePolicyEnforcerIT.java
+++ security-keycloak-authorization-quickstart/src/test/java/org/acme/security/keycloak/authorization/NativePolicyEnforcerIT.java
@@ -2,9 +2,6 @@ package org.acme.security.keycloak.authorization;
 
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
-/**
- * @author <a href="mailto:psilva@redhat.com">Pedro Igor</a>
- */
 @QuarkusIntegrationTest
 public class NativePolicyEnforcerIT extends PolicyEnforcerTest {
 }
